d327522fff0a8e65c4f05e84e788d8391d755be8
Ro <orinarhomill@gmail.com>
2022-06-11 02:38:47 -0500

    Directory Changes
    Made some directory changes and made a lot of new sections and completed a few units.

---
+++ Section 3/Conditional If Statement/main.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main()
+{
+  int num1 = 0;
+
+  if(num1 == 0)
+    {
+      print("It is 0");
+    }
+  else if(num1 == 10)
+  {
+    print(" It is 10");
+  }
+  else if(num1 > 50)
+    {
+      print("Greater than 50");
+    }
+  else
+    {
+      print("Default");
+    }
+}
