8e9b4ccb937b1c2cee8524193ffed95bf1db15e0
Ro <orinarhomill@gmail.com>
2022-06-12 03:18:23 -0500

    Finished a few parts in section 3
    I finished a few sections in section 3 of this module.

---
+++ Section 3/Switch Statement/main.dart
@@ -0,0 +1,79 @@
+import 'dart:io';
+
+void main()
+{
+  int x = 2;
+
+  switch(x)
+  {
+    case 0:
+        print("0");
+        break;
+
+    case 01:
+      print("1");
+      break;
+
+    case 2:
+      print("2");
+      break;
+
+    case 3:
+      print("3");
+      break;
+
+    case 4:
+      print("4");
+      break;
+
+    case 5:
+      print("5");
+      break;
+
+    case 6:
+      print("6");
+      break;
+
+    case 7:
+      print("7");
+      break;
+
+    default:
+      print("default");
+      break;
+  }
+
+  //equals
+
+
+  /*
+  if(x == 0)
+    {
+      print("Is 0");
+    }
+  else if(x == 1)
+    {
+      print("Is 1");
+    }
+  else if(x == 2)
+  {
+    print("Is 2");
+  }
+  else if(x == 3)
+  {
+    print("Is 3");
+  }
+  else if(x == 4)
+  {
+    print("Is 4");
+  }
+  else if(x == 5)
+  {
+    print("Is 5");
+  }
+  else
+    {
+      print("Default");
+    }
+  */
+}
