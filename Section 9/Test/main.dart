a37993984ec01d34d44607fde20949f210c204a0
Ro <orinarhomill@gmail.com>
2022-07-24 21:43:57 -0500

    Completed Course

---
+++ Section 9/Test/main.dart
@@ -0,0 +1,16 @@
+import 'dart:collection';
+import 'dart:io';
+
+import '../../Section 4/Function Return Values/main.dart';
+
+void main()
+{
+ Add(5, 7);
+ Add(-9, 2);
+ Add(99, 0);
+}
+
+void Add(int num1, int num2)
+{
+ print(num1 + num2);
+}
